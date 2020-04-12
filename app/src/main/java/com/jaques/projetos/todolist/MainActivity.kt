ccaccd15ca6ad8ba07345294360f8b07a434b10a
Alex Leko <lekodeveloperweb@hotmail.com>
2020-04-12 20:05:56 -0300

    fix cast error on recyclerView fix height of todo_list items

--- app/src/main/java/com/jaques/projetos/todolist/MainActivity.kt
+++ app/src/main/java/com/jaques/projetos/todolist/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.jaques.projetos.todolist
 
+import android.os.Build
 import android.os.Bundle
-import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jaques.projetos.todolist.Adapter.TodoListAdapter
 import com.jaques.projetos.todolist.model.Todo
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
         val todos: ArrayList<Todo> = ArrayList()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         todos.add(Todo("Comprar Carne", false))
         todos.add(Todo("Comprar Legume", false))
-        todos.add(Todo("Comprar Coffee", false))
+        todos.add(Todo("Comprar Coffee", true, LocalDate.now()))
 
-        val recycleView= R.id.recyclerView as RecyclerView
+        val recycleView = findViewById<RecyclerView>(R.id.recyclerView)
         recycleView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,false)
 
         val adapter = TodoListAdapter(todos)
